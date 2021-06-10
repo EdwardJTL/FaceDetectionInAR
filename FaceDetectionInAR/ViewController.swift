c48f7bedd861f7be5edfcc2ee8628ebc041f17b1
Edward Luo <edward.luo@mail.utoronto.ca>
2021-06-10 13:50:26 -0400

    Make Vision Request

--- FaceDetectionInAR/ViewController.swift
+++ FaceDetectionInAR/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBOutlet weak var scanButton: UIButton!
+
+    var isDetectionAllowed: Bool = true
+    var sourceImage: CGImage?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,6 +43,54 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+
+    // MARK: - Button Methods
+    @IBAction func scanButtonPressed(_ sender: Any) {
+        guard isDetectionAllowed else {
+            presentAlert(title: "Please try again later")
+            return
+        }
+        detectFaces()
+    }
+
+    // MARK: - Detection Methods
+    func detectFaces() {
+        guard let capture = sceneView.session.currentFrame?.capturedImage else {
+            presentAlert(title: "No AR capture")
+            return
+        }
+        isDetectionAllowed = false
+        let ciImage = CIImage(cvPixelBuffer: capture)
+        let context = CIContext()
+
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
+            presentAlert(title: "Cant make cgImage")
+            isDetectionAllowed = true
+            return
+        }
+
+        let detectionRequestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        let faceDetectionRequest = VNDetectFaceRectanglesRequest(completionHandler: self.onFaceDetected(request:error:))
+        faceDetectionRequest.preferBackgroundProcessing = true
+        do {
+            try detectionRequestHandler.perform([faceDetectionRequest])
+        } catch {
+            isDetectionAllowed = true
+            presentAlert(title: "Error performing detection")
+        }
+    }
+
+    func onFaceDetected(request: VNRequest, error: Error?) {
+        defer { isDetectionAllowed = true }
+        let visionResults = request.results?.compactMap { $0 as? VNFaceObservation }
+        print(visionResults.debugDescription)
+    }
+
+    func presentAlert(title: String, message: String? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: nil))
+        self.present(alert, animated: true) {}
     }
 
     // MARK: - ARSCNViewDelegate
